faf993cead3802f0d1bdc6957c0372a6a0a8d1b8
redzuiaiwo <44330706@qq.com>
2025-03-11 01:14:15 +0800

    Update zdiy.js

--- zdiy.js
+++ zdiy.js
@@ -1,3 +1,4 @@
 [rewrite_local]
 ^https:\/\/api\.dragonlongzhu\.cn\/.* url script-response-body https://raw.githubusercontent.com/redzuiaiwo/Cxx/refs/heads/main/zzmusicplay.js.txt
+[mitm] 
 hostname = api.dragonlongzhu.cn
